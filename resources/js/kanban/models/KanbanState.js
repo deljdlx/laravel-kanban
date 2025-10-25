816f1dbf5c3257ee58f36d817f73e73dc9786544
julien delsescaux <jdelsescaux@stimdata.com>
2025-10-25 15:36:48 +0200

    edition ticket

--- resources/js/kanban/models/KanbanState.js
+++ resources/js/kanban/models/KanbanState.js
@@ -263,10 +263,9 @@ class KanbanState {
         // Conserve l’instance Ticket pour garantir toJSON
         const updated = new Ticket(found.ticket.title, { ...found.ticket, ...data });
 
-        console.group('%cKanbanState.js :: 266 =============================', 'color: #f12897; font-size: 1rem');
-        console.log(data);
-        console.log(updated);
-        console.groupEnd();
+        if(Array.isArray(data.comments)) {
+            updated.comments = data.comments;
+        }
 
         found.col.tickets[found.idx] = updated;
         await this.persist({ op: 'updateTicket', ticketId: id, columnId: found.col.id, ticket: updated });
